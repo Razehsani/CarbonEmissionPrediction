828bbd3dd4e7d12de4ba631d6de70ece9a1244be
RaziehEhsani <ehsanirazieh209@gmail.com>
2024-03-14 22:11:35 +0000

    add comment

--- utils.py
+++ utils.py
@@ -10,9 +10,9 @@ def load_dataset(dataset_name: str, cft_evaluate: bool, experiment_type: str):
     elif (dataset_name.endswith("xlsx")):
         dataset = pd.read_excel(dataset_name)
 
-    dataset = dataset.fillna(0)
+    dataset = dataset.fillna(0) #impute #transformation
 
-    # Normalizing [Cultivated area (hectares)] feature
+    # Normalizing [Cultivated area (hectares)] feature #droping cultivated area # transformation
     if ("Cultivated area (hectares)" in dataset.columns):
         for column in dataset.columns:
             if (column not in constants.excluded_normalization_columns):
@@ -32,7 +32,7 @@ def load_dataset(dataset_name: str, cft_evaluate: bool, experiment_type: str):
     X = dataset.iloc[:, independent_variables].values
     y = dataset.iloc[:, dependent_variable].values if cft_evaluate == False else dataset.iloc[:, cft_dependant_variable].values
 
-    # Scaling input and output variables features
+    # Scaling input and output variables features as a transformation
     scaler = MinMaxScaler(feature_range=(0, 1))
     y_scaled = scaler.fit_transform(y.reshape(-1, 1))
     X_scaled = scaler.fit_transform(X)
